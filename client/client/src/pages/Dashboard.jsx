29f1122e92ce88eeaafb8a54d50694b68a06f430
Deepthi Das <das.deepthi1201@gmail.com>
2026-03-05 07:34:00 +0530

    dashboard updated

--- client/client/src/pages/Dashboard.jsx
+++ client/client/src/pages/Dashboard.jsx
@@ -401,7 +401,7 @@ function Dashboard() {
   const token = localStorage.getItem("token");
   const socket = io("http://localhost:5000");
   const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://ticket-system-ashen-pi.vercel.app/",
     headers: { Authorization: `Bearer ${token}` }
   });
 
